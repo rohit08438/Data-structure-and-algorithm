f17660391cff31bb231d903e937ab7a14c0fb8a7
aarush <rohitsingh7802@gmail.com>
2026-08-14 21:27:19 +0530

    insertion sorting

---
+++ insertion sort.py
@@ -0,0 +1,10 @@
+nums=[3,5,6,4,8,9,10,7,1]
+n=len(nums)
+for i in range(1,n):
+    key=nums[i]
+    j=i-1
+    while j>=0 and key<nums[j]:
+        nums[j+1]=nums[j]
+        j-=1
+    nums[j+1]=key
+print(nums)
