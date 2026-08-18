4b9e60ba597a6bbdbe169679abb6d0aecd28ee13
aarush <rohitsingh7802@gmail.com>
2026-08-18 13:43:41 +0530

    largest using max and if

---
+++ largest element in a array.py
@@ -0,0 +1,13 @@
+nums=[55,32,-77,0,12,-5,99]
+largest=nums[0]
+n=len(nums)
+for i in range(0,n):
+    #if nums[i]>largest:
+        #largest=nums[i]
+    largest=max(largest,nums[i])
+
+print(largest)
+
+
+
+
